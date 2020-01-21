92a78220a42661470c8623ff37347ffcc81d837c
Caszandra Gray Strider <cstrider@shay.ecn.purdue.edu>
2020-01-20 19:55:53 -0500

    stuff

--- cryptBreak.py
+++ cryptBreak.py
@@ -1,8 +1,8 @@
-
-# coding: utf-8
-
-# In[3]:
-
+#!/usr/bin/env python
+import os
+#import subprocess
+from subprocess import *
+import pexpect
 #Arguments:
 # ciphertextFile: String containing file name of the ciphertext (e.g. encrypted.txt )
 # key_bv: 16-bit BitVector of the key used to try to decrypt the ciphertext.
@@ -11,12 +11,25 @@
 #  the original plaintext as a string
 def cryptBreak(ciphertextFile,key_bv):
     #DecryptForFun.py ciphertextFile output.txt
-    exec('DecryptForFun.py ciphertextFile output.txt')
-    
-    
+    #exec('DecryptForFun.py ciphertextFile output.txt')
+	#os.system("DecryptForFun.py encrypted.txt output.txt")
+	#child = pexpect.spawn('python DecryptForFun.py encrypted.txt output.txt')
+	#child.expect("\nEnter key: ")
+	#child.sendline(key_bv)
+	test_str = str(key_bv)
+	child = Popen(['python', 'DecryptForFun.py', 'encrypted.txt', 'output.txt'], stdin=PIPE)
+	child.communicate(test_str)#key_bv)
+	FILEIN = open('output.txt', 'r')
+	contents = FILEIN.read()
+	FILEIN.close()
+	return contents
+	
 
-
-# In[ ]:
-
-
-
+#from BitVector import*
+#someRandomInteger = 9999
+#key_bv = BitVector(intVal=someRandomInteger, size=16)
+#decryptedMessage = cryptBreak('encrypted.txt', key_bv)
+#if 'Mark Twain' in decryptedMessage:
+#	print('Encryption Broken!')
+#else:
+#	print('Not decrypted yet')
--- test.py
+++ test.py
@@ -1,21 +1,18 @@
-
-# coding: utf-8
-
-# In[6]:
-
-#import cryptBreak
-from cryptBreak.py import *
+#!/usr/bin/env python
+import cryptBreak
+from cryptBreak import *
 from BitVector import *
-someRandomInteger = 9999 #Arbitrary integer for creating a BitVector
-key_bv = BitVector(intVal=someRandomInteger, size=16)
-decryptedMessage = cryptBreak.cryptBreak('encrypted.txt', key_bv)
-if 'Mark Twain' in decryptedMessage:
-    print('Encryption Broken!')
-else:
-    print('Not decrypted yet')
-
-
-# In[ ]:
-
-
-
+is_done = 0
+num = 16765#0
+while(is_done == 0 & num < 65535):
+	someRandomInteger = num#9999 #Arbitrary integer for creating a BitVector
+	key_bv = BitVector(intVal=someRandomInteger, size=16)
+	decryptedMessage = cryptBreak('encrypted.txt', key_bv)
+	#print("decryptedMessage = ", decryptedMessage)
+	if ('Mark Twain' in decryptedMessage):
+		print('Encryption Broken!')
+		is_done = 1
+	else:
+		print('Not decrypted yet')
+	num += 1
+	print(num)
